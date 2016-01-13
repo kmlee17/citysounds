f2e8176afb46683ba34def609d7496cbc7afaf04
kmlee17 <kmlee1717@yahoo.com>
2016-01-13 08:51:23 -0800

    code cleanup

--- app/predict_sound.py
+++ app/predict_sound.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 import cPickle
 import librosa as lr
-# import aubio as ab
 from scipy.stats import skew, kurtosis
 from sklearn.preprocessing import normalize, StandardScaler
 
--- code/feature_extraction.py
+++ code/feature_extraction.py
@@ -4,15 +4,12 @@ from setup import LOCAL_REPO_DIR
 import numpy as np
 import pandas as pd
 import librosa as lr
-import aubio as ab
 from scipy.stats import skew, kurtosis
-from sklearn.manifold import MDS
 import scipy.io.wavfile as wav
 
 # variables for the mfcc extraction
 
-# fft window size: so if sample rate is 44.1 kHz,
-# a window of 1024 is 0.23 ms
+# fft window size: ex. sample rate is 44.1 kHz, a window of 1024 is 0.23 ms
 window = 1024
 # hop size: how far to jump ahead to the next window
 hop = window / 2
@@ -23,8 +20,8 @@ n_coeffs = 25
 # sample rate of the audio before transformation into the frequency domain
 sample_rate = 44100
 
-df_path = LOCAL_REPO_DIR + 'metadata/citysound.csv'
-df = pd.read_csv(df_path)
+# df_path = LOCAL_REPO_DIR + 'metadata/citysound.csv'
+# df = pd.read_csv(df_path)
 
 # testing first fold only
 # df_f1 = df[df['fold'] == 1]
@@ -69,43 +66,91 @@ y = []
 #     X.append(features)
 #     y.append(row['class'])
 
-# librosa
-for i, row in df.iterrows():
-    wavfile = LOCAL_REPO_DIR + 'audio/fold' + str(row['fold']) + '/' + row['slice_file_name']
+def extract_features(csv_path):
+    '''
+    INPUT:
+    - A csv file with the following labeled columns:
+    - 'slice_file_name': filename of the audio sample
+    - 'class': class of audio sample
+    - 'fold': numerical folder name of where the file is stored (ie '1' -> in folder 'fold1')
 
-    # print statements to update the progress of the processing
-    print wavfile
-    print i
+    OUTPUT:
+    - Saves feature csv with 'class' and 'fold' data included in location specified.  Each
+      row is a feature vector (275 features) representing an audio sample
 
-    wav_mat, sr = lr.load(wavfile, sr=sample_rate)
-    S = lr.feature.melspectrogram(wav_mat, sr=sr, n_mels=n_filters, fmax=sr/2, n_fft=window, hop_length=hop)
-    mfcc = lr.feature.mfcc(S=lr.logamplitude(S), n_mfcc=25)
-    # mfcc = lr.feature.mfcc(wav_mat, sr, n_mfcc=25, n_fft=1024, hop_length=512, n_mels=40, fmax=22050)
-    # mfcc = lr.feature.mfcc(wav_mat, sr, n_mfcc=13)
-    mfcc_delta = lr.feature.delta(mfcc)
-    mfcc_delta = mfcc_delta.T
-    mfcc_delta2 = lr.feature.delta(mfcc, order=2)
-    mfcc_delta2 = mfcc_delta2.T
-    mfcc = mfcc.T
-    total_mfcc = np.column_stack((mfcc, mfcc_delta, mfcc_delta2))
-    avg_mfcc = np.mean(total_mfcc, axis=0)
-    var_mfcc = np.var(total_mfcc, axis=0)
-    max_mfcc = np.max(mfcc, axis=0)
-    min_mfcc = np.min(mfcc, axis=0)
-    med_mfcc = np.median(mfcc, axis=0)
-    skew_mfcc = skew(mfcc, axis=0)
-    kurt_mfcc = skew(mfcc, axis=0)
-    features = np.concatenate((avg_mfcc, var_mfcc, max_mfcc, min_mfcc, med_mfcc, skew_mfcc, kurt_mfcc))
-    X.append(features)
-    y.append(row['class'])
+    '''
+    
+    df = pd.read_csv(df_path)
 
-X = np.asarray(X)
-y = np.asarray(y)
+    # iterate through every row in the dataframe, each row represents an audio sample
+    for i, row in df.iterrows():
 
-mono = pd.DataFrame(X)
-mono['class'] = y
-mono['fold'] = df['fold']
+        # construct the path of the file
+        wavfile = LOCAL_REPO_DIR + 'audio/fold' + str(row['fold']) + '/' + row['slice_file_name']
+    
+        # print statements to update the progress of the processing
+        print wavfile
+        print i
+        
+        # load the raw audio .wav file as a matrix using librosa
+        wav_mat, sr = lr.load(wavfile, sr=sample_rate)
 
-mono.to_csv('csv/urbansound_stereo.csv')
+        # create the spectrogram using the predefined variables for mfcc extraction
+        S = lr.feature.melspectrogram(wav_mat, sr=sr, n_mels=n_filters, fmax=sr/2, n_fft=window, hop_length=hop)
 
+        # using the pre-defined spectrogram, extract the mfcc coefficients
+        mfcc = lr.feature.mfcc(S=lr.logamplitude(S), n_mfcc=25)
+        
+        # calculate the first and second derivatives of the mfcc coefficients to detect changes and patterns
+        mfcc_delta = lr.feature.delta(mfcc)
+        mfcc_delta = mfcc_delta.T
+        mfcc_delta2 = lr.feature.delta(mfcc, order=2)
+        mfcc_delta2 = mfcc_delta2.T
+        mfcc = mfcc.T
+
+        # combine the mfcc coefficients and their derivatives in a column stack for analysis
+        total_mfcc = np.column_stack((mfcc, mfcc_delta, mfcc_delta2))
+
+        # use the average of each column to condense into a feature vector
+        # this makes each sample uniform regardless of the length of original the audio sample
+        # the following features are extracted
+        # - avg of mfcc, first derivative, second derivative
+        # - var of mfcc, first derivative, second derivative
+        # - max of mfcc
+        # - min of mfcc
+        # - median of mfcc
+        # - skew of mfcc
+        # - kurtosis of mfcc
+        avg_mfcc = np.mean(total_mfcc, axis=0)
+        var_mfcc = np.var(total_mfcc, axis=0)
+        max_mfcc = np.max(mfcc, axis=0)
+        min_mfcc = np.min(mfcc, axis=0)
+        med_mfcc = np.median(mfcc, axis=0)
+        skew_mfcc = skew(mfcc, axis=0)
+        kurt_mfcc = skew(mfcc, axis=0)
+
+        # combine into one vector and append to the total feature matrix
+        features = np.concatenate((avg_mfcc, var_mfcc, max_mfcc, min_mfcc, med_mfcc, skew_mfcc, kurt_mfcc))
+        X.append(features)
+
+    # extract the class of the sample
+    # y.append(row['class'])
+    X = np.asarray(X)
+    features = pd.DataFrame(X)
+    features['class'] = y
+    features['fold'] = df['fold']
+    features.to_csv('csv/citysounds_test.csv')
+
+# X = np.asarray(X)
+# y = np.asarray(y)
+
+# features = pd.DataFrame(X)
+# features['class'] = y
+# features['fold'] = df['fold']
+
+# features.to_csv('csv/urbansound_stereo.csv')
+
+if __name__ == '__main__':
+    csv_path = LOCAL_REPO_DIR + 'metadata/citysounds_meta.csv'
+    extract_features(csv_path)    
 
