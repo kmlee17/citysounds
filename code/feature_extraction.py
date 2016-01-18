4975113bfc743f580850d73e176ac681f72b5c67
kmlee17 <kmlee1717@yahoo.com>
2016-01-18 12:39:17 -0800

    replaces iterrows with apply function

--- code/feature_extraction.py
+++ code/feature_extraction.py
@@ -21,6 +21,63 @@ n_coeffs = 25
 sample_rate = 44100
 
 
+def single_file_featurization(row):
+    '''
+    INPUT:
+    row of dataframe with 'audio_slice_name' as the filename of the audio sample
+
+    OUTPUT:
+    feature vector for audio sample
+    
+    Function for dataframe apply for extracting each audio sample into a feature vector
+    of mfcc coefficients
+    '''
+
+    wavfile = LOCAL_REPO_DIR + 'audio/fold' + str(row['fold']) + '/' + row['slice_file_name']
+    
+    # print statements to update the progress of the processing
+    print wavfile
+
+    # load the raw audio .wav file as a matrix using librosa
+    wav_mat, sr = lr.load(wavfile, sr=sample_rate)
+
+    # create the spectrogram using the predefined variables for mfcc extraction
+    S = lr.feature.melspectrogram(wav_mat, sr=sr, n_mels=n_filters, fmax=sr/2, n_fft=window, hop_length=hop)
+
+    # using the pre-defined spectrogram, extract the mfcc coefficients
+    mfcc = lr.feature.mfcc(S=lr.logamplitude(S), n_mfcc=25)
+    
+    # calculate the first and second derivatives of the mfcc coefficients to detect changes and patterns
+    mfcc_delta = lr.feature.delta(mfcc)
+    mfcc_delta = mfcc_delta.T
+    mfcc_delta2 = lr.feature.delta(mfcc, order=2)
+    mfcc_delta2 = mfcc_delta2.T
+    mfcc = mfcc.T
+
+    # combine the mfcc coefficients and their derivatives in a column stack for analysis
+    total_mfcc = np.column_stack((mfcc, mfcc_delta, mfcc_delta2))
+
+    # use the average of each column to condense into a feature vector
+    # this makes each sample uniform regardless of the length of original the audio sample
+    # the following features are extracted
+    # - avg of mfcc, first derivative, second derivative
+    # - var of mfcc, first derivative, second derivative
+    # - max of mfcc
+    # - min of mfcc
+    # - median of mfcc
+    # - skew of mfcc
+    # - kurtosis of mfcc
+    avg_mfcc = np.mean(total_mfcc, axis=0)
+    var_mfcc = np.var(total_mfcc, axis=0)
+    max_mfcc = np.max(mfcc, axis=0)
+    min_mfcc = np.min(mfcc, axis=0)
+    med_mfcc = np.median(mfcc, axis=0)
+    skew_mfcc = skew(mfcc, axis=0)
+    kurt_mfcc = skew(mfcc, axis=0)
+
+    # combine into one vector and append to the total feature matrix
+    return np.concatenate((avg_mfcc, var_mfcc, max_mfcc, min_mfcc, med_mfcc, skew_mfcc, kurt_mfcc))
+
 def extract_features(csv_path):
     '''
     INPUT:
@@ -38,59 +95,11 @@ def extract_features(csv_path):
     '''
     
     df = pd.read_csv(csv_path)
-    X = []
+    df = df.head(10)
+    # X = []
     y = []
 
-    # iterate through every row in the dataframe, each row represents an audio sample
-    for i, row in df.iterrows():
-
-        # construct the path of the file
-        wavfile = LOCAL_REPO_DIR + 'audio/fold' + str(row['fold']) + '/' + row['slice_file_name']
-    
-        # print statements to update the progress of the processing
-        print wavfile
-        print i
-        
-        # load the raw audio .wav file as a matrix using librosa
-        wav_mat, sr = lr.load(wavfile, sr=sample_rate)
-
-        # create the spectrogram using the predefined variables for mfcc extraction
-        S = lr.feature.melspectrogram(wav_mat, sr=sr, n_mels=n_filters, fmax=sr/2, n_fft=window, hop_length=hop)
-
-        # using the pre-defined spectrogram, extract the mfcc coefficients
-        mfcc = lr.feature.mfcc(S=lr.logamplitude(S), n_mfcc=25)
-        
-        # calculate the first and second derivatives of the mfcc coefficients to detect changes and patterns
-        mfcc_delta = lr.feature.delta(mfcc)
-        mfcc_delta = mfcc_delta.T
-        mfcc_delta2 = lr.feature.delta(mfcc, order=2)
-        mfcc_delta2 = mfcc_delta2.T
-        mfcc = mfcc.T
-
-        # combine the mfcc coefficients and their derivatives in a column stack for analysis
-        total_mfcc = np.column_stack((mfcc, mfcc_delta, mfcc_delta2))
-
-        # use the average of each column to condense into a feature vector
-        # this makes each sample uniform regardless of the length of original the audio sample
-        # the following features are extracted
-        # - avg of mfcc, first derivative, second derivative
-        # - var of mfcc, first derivative, second derivative
-        # - max of mfcc
-        # - min of mfcc
-        # - median of mfcc
-        # - skew of mfcc
-        # - kurtosis of mfcc
-        avg_mfcc = np.mean(total_mfcc, axis=0)
-        var_mfcc = np.var(total_mfcc, axis=0)
-        max_mfcc = np.max(mfcc, axis=0)
-        min_mfcc = np.min(mfcc, axis=0)
-        med_mfcc = np.median(mfcc, axis=0)
-        skew_mfcc = skew(mfcc, axis=0)
-        kurt_mfcc = skew(mfcc, axis=0)
-
-        # combine into one vector and append to the total feature matrix
-        features = np.concatenate((avg_mfcc, var_mfcc, max_mfcc, min_mfcc, med_mfcc, skew_mfcc, kurt_mfcc))
-        X.append(features)
+    X = df.apply(lambda x: pd.Series(single_file_featurization(x)), axis=1)
 
     X = np.asarray(X)
     features = pd.DataFrame(X)
