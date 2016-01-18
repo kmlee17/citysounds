bc0988878978fcee19f40723ea08bfb58a28eaa3
kmlee17 <kmlee1717@yahoo.com>
2016-01-18 13:48:40 -0800

    clean up grid_search.py

--- code/grid_search.py
+++ code/grid_search.py
@@ -12,6 +12,12 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.cross_validation import LeaveOneLabelOut
 
+'''
+USAGE:
+- run from command line (i.e. 'python code/grid_search.py knn')
+- choose between 'randomforest', 'svm', 'knn', 'gradientboost'
+'''
+
 data_path = LOCAL_REPO_DIR + 'csv/citysounds.csv'
 
 def get_features_classes():
@@ -36,35 +42,30 @@ def get_features_classes():
 def svm_grid_search():
     params_svm = [{'C': [1, 0.75, 0.5, 0.4, 0.3, 0.25], 'gamma': [0.1, 0.075, 0.05, 0.04, 0.03, 0.025, 0.01]}]
     svm = SVC()
-    X, y, lolo = get_features_classes()
-    gs = GridSearchCV(svm, params_svm, cv=lolo, verbose=3, scoring='accuracy')
-    gs.fit(X, y)
-    print gs.best_params_
-    print gs.best_score_
+    grid_search(svm, params)
+
 
 def knn_grid_search():
-    params_knn = [{'n_neighbors': [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]}]
+    params = [{'n_neighbors': [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]}]
     knn = KNeighborsClassifier()
-    X, y, lolo = get_features_classes()
-    gs = GridSearchCV(knn, params_knn, cv=lolo, verbose=3, scoring='accuracy')
-    gs.fit(X, y)
-    print gs.best_params_
-    print gs.best_score_
+    grid_search(knn, params)
+
 
 def rf_grid_search():
-    params_rf = [{'n_estimators': [10, 50, 100, 250, 500], 'criterion': ['gini', 'entropy']}]
+    params = [{'n_estimators': [10, 50, 100, 250, 500], 'criterion': ['gini', 'entropy']}]
     rf = RandomForestClassifier()
-    X, y, lolo = get_features_classes()
-    gs = GridSearchCV(rf, params_rf, cv=lolo, verbose=3, scoring='accuracy')
-    gs.fit(X, y)
-    print gs.best_params_
-    print gs.best_score_
+    grid_search(rf, params)
+
 
 def gb_grid_search():
-    params_gb = [{'learning_rate': [0.1, 0.01, 0.25, 0.5], 'n_estimators': [100], 'max_depth': [3, 5]}]
+    params = [{'learning_rate': [0.1, 0.01, 0.25, 0.5], 'n_estimators': [100], 'max_depth': [3, 5]}]
     gb = GradientBoostingClassifier()
+    grid_search(gb, params)
+
+
+def grid_search(model, params):
     X, y, lolo = get_features_classes()
-    gs = GridSearchCV(gb, params_gb, cv=lolo, verbose=3, scoring='accuracy')
+    gs = GridSearchCV(model, params, cv=lolo, verbose=3, scoring='accuracy')
     gs.fit(X, y)
     print gs.best_params_
     print gs.best_score_
